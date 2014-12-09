a164ce5664e30b85bdc2b9838098aff862bb60c2
eliadil <ginafff@gmail.com>
2014-12-09 15:20:05 +0100

    Prettyfying console log

--- src/us/corenetwork/tradecraft/Logs.java
+++ src/us/corenetwork/tradecraft/Logs.java
@@ -1,33 +1,40 @@
 package us.corenetwork.tradecraft;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 
 public class Logs
 {
 	public static void debug(String text)
 	{
 		if (Settings.getBoolean(Setting.DEBUG))
-			info(text);
+			sendLog("&f[&3TradeCraft&f]&f " + text);
 	}
 
-    public static void debugIngame(String text)
-    {
-        if (Settings.getBoolean(Setting.DEBUG))
-            Bukkit.broadcastMessage(text);
-    }
-	
+	public static void debugIngame(String text)
+	{
+		if (Settings.getBoolean(Setting.DEBUG))
+			Bukkit.broadcastMessage(text);
+	}
+
 	public static void info(String text)
 	{
-		Bukkit.getLogger().info("[TradeCraft] " + text);
+		sendLog("&f[&fTradeCraft&f]&f "+text);
 	}
-	
+
 	public static void warning(String text)
 	{
-		Bukkit.getLogger().warning("[TradeCraft] " + text);
+		sendLog("&f[&eTradeCraft&f]&f " + text);
 	}
-	
+
 	public static void severe(String text)
 	{
-		Bukkit.getLogger().severe("[TradeCraft] " + text);
+		sendLog("&f[&cTradeCraft&f]&f " + text);
 	}
+
+	public static void sendLog(String text)
+	{
+		Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', text));
+	}
+
 }
